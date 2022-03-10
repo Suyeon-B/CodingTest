4810cbcf42a0ea443322a5e244ee08d1f36cd567
Suyeon-B <tndus4243@naver.com>
2022-03-10 22:48:19 +0900

    코드업 기초 6001-6010

--- CodeUp100.py
+++ CodeUp100.py
@@ -10,3 +10,25 @@ print("Hello World")
 print("Hello\nWorld")
 
 # 6004
+print("'Hello'")
+
+# 6005
+print('"Hello World"')
+
+# 6006
+print('"'"!@#$%^&*()'")
+# 또는 print("\"!@#$%^&*()'")
+
+# 6007
+print("\"C:\\Download\\'hello'.py\"")
+
+# 6008
+print('print("Hello\\nWorld")')
+
+# 6009
+c = input()
+print(c)
+
+# 6010
+n = int(input())
+print(n)
