f22921b81953b6dc98f930d899b8733983902b70
Suyeon-B <tndus4243@naver.com>
2022-03-18 22:23:06 +0900

    빈도수 rank 조정 필요

--- Baekjoon/Greedy/1339) 단어 수학.py
+++ Baekjoon/Greedy/1339) 단어 수학.py
@@ -1,13 +1,24 @@
 def alpha_math(alpha_with_rank):
     points = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    # 자릿값으로 1차 rank 정하기
     sorted_alpha_rank = sorted(alpha_with_rank.items(), key = lambda item: item[1][0], reverse = True)
 
     alphas = []
     ranks = []
 
+    # 빈도수로 2차 rank 정하기
+    freq = []
+    for i in range(len(ranks)):
+        freq.append(len(ranks[i]))
+
+    if len(freq) != set(freq):
+
+
     for i in range(len(sorted_alpha_rank)):
         alphas.append(sorted_alpha_rank[i][0])
         ranks.append(sorted_alpha_rank[i][1])
+
+
 
     answer = 0
 
@@ -16,23 +27,26 @@ def alpha_math(alpha_with_rank):
         for j in range(len(ranks[i])):
             answer += point*(10**(ranks[i][j]-1))
 
-    return answer
-
+    print(answer)
 
 
 N = int(input())
-alpha_with_rank = {}
-digit_sum = 0
-for i in range(N):
-    alpha = input()
-    alpha_digit = len(alpha)
-    digit_sum += alpha_digit
-    for j in range(alpha_digit):
-        if alpha[j] in alpha_with_rank:
-            alpha_with_rank[alpha[j]].append(alpha_digit - j)
-            alpha_with_rank[alpha[j]].sort(reverse=True)
-        else:
-            alpha_with_rank[alpha[j]] = [alpha_digit - j]
+if N == 1:
+    points = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    for i in range(len(input())):
+        print(points.pop(), end="")
+else:
+    alpha_with_rank = {}
+    digit_sum = 0
+    for i in range(N):
+        alpha = input()
+        alpha_digit = len(alpha)
+        digit_sum += alpha_digit
+        for j in range(alpha_digit):
+            if alpha[j] in alpha_with_rank:
+                alpha_with_rank[alpha[j]].append(alpha_digit - j)
+                alpha_with_rank[alpha[j]].sort(reverse=True)
+            else:
+                alpha_with_rank[alpha[j]] = [alpha_digit - j]
 
-
-print(alpha_math(alpha_with_rank))
+    alpha_math(alpha_with_rank)
