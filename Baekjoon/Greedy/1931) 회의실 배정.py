0de6fcfdf1cf694beebeff3c7daaeeb7af8a59fa
Suyeon-B <tndus4243@naver.com>
2022-03-23 22:00:03 +0900

    1931 회의실 배정 해결

--- Baekjoon/Greedy/1931) 회의실 배정.py
+++ Baekjoon/Greedy/1931) 회의실 배정.py
@@ -1,33 +1,29 @@
 """
-1. 첫 번째 숫자로 정렬
-2. 텀으로 정렬
+1. 회의 시작 시간(s)으로 정렬 후
+2. 회의 끝 시간(e)으로 정렬
 
-1번 기준으로 순회하며
-2번 기준으로 선착순 선택
+정렬된 리스트를 기준으로 e <= s일 때 추가해줌
 """
 
 def meetingRoom(N, meetingTimes):
-    sortedByStart = meetingTimes.sort(key=lambda x:x[0])
-    sortedByTerm = meetingTimes.sort(key=lambda x:x[-1])
+    meetingTimes = sorted(meetingTimes, key=lambda x:x[0])
+    meetingTimes = sorted(meetingTimes, key=lambda x:x[1])
 
-    answer = []
-    for i in range(N):
-        for j in range(N):
-            if sortedByStart[i] == sortedByTerm[j]:
-                if answer[-1][-1] < sortedByStart[i]:
-                    answer.append(sortedByStart[i])
+    answer = 1
+    last = meetingTimes[0][1]
+    for i in range(N-1):
+        if last <= meetingTimes[i+1][0]:
+            last = meetingTimes[i+1][1]
+            answer+=1
 
-    return len(answer)
+    return answer
 
 N = int(input())
 meetingTimes = []
-term = 0
 for i in range(N):
     mts = []
     for j in range(1):
         s, e = map(int, input().split())
-        term = abs(s - e)
-        mts.append([s, e, term])
-    meetingTimes.append(mts)
+        meetingTimes.append([s, e])
 
 print(meetingRoom(N, meetingTimes))
