5652087b396e00632c0f1c2fe447b84402bffbb8
Suyeon-B <tndus4243@naver.com>
2022-04-26 21:10:15 +0900

    11497, 2133

---
+++ Baekjoon/DP/2133 타일 채우기.py
@@ -0,0 +1,17 @@
+from sys import stdin
+stdin = open("Baekjoon/DP/input.txt",'r')
+input = stdin.readline
+
+n = int(input().strip())
+tiles = [1]*31
+tiles[2] = 3
+if n%2 == 1:
+    print(0)
+    exit(0)
+
+for i in range(4, n+1):
+    tiles[i] = tiles[2] * tiles[i-2]
+    for j in range(i-4, -1, -2):
+        tiles[i] += tiles[j] * 2
+
+print(tiles[n])
