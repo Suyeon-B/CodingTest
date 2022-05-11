fc0861850a073913cf437853c93da095962a12d3
Suyeon-B <tndus4243@naver.com>
2022-05-12 00:20:21 +0900

    programmers level2

---
+++ programmers/멀쩡한 사각형.py
@@ -0,0 +1,24 @@
+import math
+
+
+def solution(w, h):
+    answer = 0
+
+    def recur(w, h):
+        nonlocal answer
+
+        if w == 1 or h == 1:
+            return
+
+        answer += 2 * (w//2) * (h//2)
+        left_up = recur(math.ceil(w/2), math.ceil(h/2))
+        right_up = recur(math.ceil(w/2), math.ceil(h/2))
+        if left_up and right_up:
+            answer = left_up+right_up
+
+    recur(w, h)
+    return answer
+
+
+w, h = 3, 3
+print(solution(w, h))
