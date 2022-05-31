75ff3fcc009d028e4c0b275b08e004a78e5e5eac
Suyeon-B <tndus4243@naver.com>
2022-05-31 12:46:22 +0900

    programmers

--- programmers/가장 큰 수 copy.py
+++ programmers/가장 큰 수 copy.py
@@ -1,6 +1,11 @@
 def solution(numbers):
-    
-    
-
-numbers = [3, 30, 34, 5, 9]
-print(solution(numbers))
+    """
+    자리수 비교를 위해 최대공약수 길이 만큼 확장한 뒤, 정렬해서 이어붙인다.
+    """
+    answer = ''
+    strnum = list(map(str, numbers))
+    gdb_length = [[i, i * (12//len(i))] for i in strnum]
+    gdb_length.sort(key=lambda x:x[1], reverse=True)
+    for i in gdb_length: answer += i[0]
+    answer = str(int(answer))
+    return answer
