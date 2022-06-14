8f41b15a29b3ff0a3cef2ccd8d6b8b4e4c462b6a
Suyeon-B <tndus4243@naver.com>
2022-06-14 10:55:09 +0900

    programmers

--- programmers/짝지어 제거하기.py
+++ programmers/짝지어 제거하기.py
@@ -4,22 +4,20 @@ from collections import deque, defaultdict
 def solution(s):
     s = deque(s)
     answer = [s.popleft()]
-    cnt = defaultdict(int)
+    # cnt = defaultdict(int)
     if len(s) < 2 or len(set(s)) > len(s)//2:
         return 0
     for _ in range(len(s)):
         now = s.popleft()
-        if cnt[now] <= 2 and answer and answer[-1] == now:
+        if answer and answer[-1] == now:
             answer.pop()
-            del cnt[now]
         else:
             answer.append(now)
-            cnt[now] += 1
 
     if len(answer) == 0:
         return 1
     else:
         return 0
 
-s = "baabaa"
+s = "abaa"
 print(solution(s))
