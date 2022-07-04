3f48e94e22e3a116998927b9a5e245c6fa222c28
Suyeon-B <tndus4243@naver.com>
2022-07-04 23:53:42 +0900

    BOJ 시간초과 개선

--- 문자열 폭발.py
+++ 문자열 폭발.py
@@ -1,12 +1,7 @@
 """
-시간초과 개선 필요
-1. 폭발 문자열이 포함되면 해당 문자열 삭제
-2. 삭제한 새로운 문자열에 폭발 문자열이 포함되면 1번으로 돌아감
-
--> 모든 폭발이 끝나고 남은 문자열 출력
-(없으면 'FRULA' 출력)
-
-(폭발 문자열은 같은 문자 포함 X)
+시간 초과 개선 버전
+1.bomb 끝 문자열과 같아지는 순간에, bomb 길이만큼 같은지 확인.
+2.bomb 문자열의 길이만큼 삭제 & 문자열 끝까지 반복
 """
 from sys import stdin
 
@@ -16,10 +11,17 @@ input = stdin.readline
 string = input().strip()
 bomb = input().strip()
 
-while string and bomb in string:
-    string = string.replace(bomb, "")
+lastChar = bomb[-1]
+stack = []
+length = len(bomb)
 
-if string:
-    print(string)
+for char in string:
+    stack.append(char)
+    if char == lastChar and "".join(stack[-length:]) == bomb:
+        del stack[-length:]
+answer = "".join(stack)
+
+if answer:
+    print(answer)
 else:
     print("FRULA")
